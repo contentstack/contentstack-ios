8d5f85ac5cbe72acba6d7e1523c7b78209751a57
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2021-09-02 12:34:35 +0530

    Fix crash on NSInvalidArgumentException for NSJson serialization
    - Added nil check for response json object

--- Script/run-test-case.sh
+++ Script/run-test-case.sh
@@ -6,4 +6,4 @@
 #  Created by Uttam Ukkoji on 09/06/20.
 #  Copyright © 2020 Contentstack. All rights reserved.
 
-set -o pipefail && env "NSUnbufferedIO=YES" xcodebuild "-workspace" "ContentStack.xcworkspace" "-scheme" "Contentstack" "build" "test" "-destination" "id=7307CFB4-C3C3-49C9-A051-FE29727D6742" | xcpretty "--color" "--report" "html" "--output" "/Users/uttamukkoji/Documents/GitHub/contentstack-ios/ContentstackTest/TestResult/xcode-test-results-Contentstack.html"
+set -o pipefail && env "NSUnbufferedIO=YES" xcodebuild "-workspace" "ContentStack.xcworkspace" "-scheme" "Contentstack" "build" "test" "-destination" "id=122C1770-2736-4869-98C2-118EF3B37850" | xcpretty "--color" "--report" "html" "--output" "/Users/uttamukkoji/Documents/GitHub/contentstack-ios/ContentstackTest/TestResult/xcode-test-results-Contentstack.html"
